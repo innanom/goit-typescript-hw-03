bd057abf76ae7465c545c0a594c3427ac06c6c08
Inna Nomerovkina <inkosof@gmail.com>
2023-12-27 16:03:11 +0200

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -20,8 +20,35 @@ class Person {
 }
 abstract class House {
     protected door: boolean = false;
+    private tenants: Person[] = [];
+
+    constructor(protected key: Key) { }
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log ("The door is opened ")
+
+        } 
+        else {
+            console.log ("The door is closed ")
+        }
+    }
     
- }
+    abstract openDoor(key: Key): void;
+}
+ 
+class MyHouse extends House {
+        openDoor(key: Key): void {
+            if (key.getSignature() === this.key.getSignature()) {
+                this.door = true;
+                console.log("The key is accepted");
+            } else {
+                this.door = false;
+                console.log("invalid key");
+            }
+
+ }}
 
 
 const key = new Key();
